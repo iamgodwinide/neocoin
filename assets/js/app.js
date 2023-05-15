8068a6fda5e51979c4881eca5fda24a8acf8f0b0
Godwin Idemudia <godwinide@gmail.com>
2023-05-15 22:18:23 +0100

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+const handleTweet = () => {
+    const tweet = `Join $NEO and his team to stop the Matrix. @neoethcoin %0A%0A`;
+    window.open(`https://twitter.com/intent/tweet?text=${tweet}&url=https://matrixcoindex.xyz`, '_blank');
+}
